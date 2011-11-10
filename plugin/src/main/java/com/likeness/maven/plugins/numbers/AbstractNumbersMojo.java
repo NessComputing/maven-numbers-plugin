9ac74d44788e734d9e83f043fa56052c72be0cfa
Henning Schmiedehausen <henning@schmiedehausen.org>
2011-11-09 16:18:53 -0800

    move object creation into base file.

--- plugin/src/main/java/com/likeness/maven/plugins/numbers/AbstractNumbersMojo.java
+++ plugin/src/main/java/com/likeness/maven/plugins/numbers/AbstractNumbersMojo.java
@@ -1,10 +1,12 @@
 package com.likeness.maven.plugins.numbers;
 
-import org.apache.maven.plugin.AbstractMojo;
-import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.project.MavenProject;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.likeness.maven.plugins.numbers.beans.DateDefinition;
 import com.likeness.maven.plugins.numbers.beans.MacroDefinition;
@@ -13,10 +15,11 @@ import com.likeness.maven.plugins.numbers.beans.PropertyGroup;
 import com.likeness.maven.plugins.numbers.beans.StringDefinition;
 import com.likeness.maven.plugins.numbers.util.Log;
 import com.pyx4j.log4j.MavenLogAppender;
-
-import java.io.File;
-import java.util.Map;
-import java.util.Properties;
+import org.apache.commons.lang3.ArrayUtils;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Base code for all the mojos.
@@ -100,6 +103,9 @@ public abstract class AbstractNumbersMojo extends AbstractMojo
 
     protected final Log LOG = Log.findLog();
 
+    private final PropertyCache propertyCache = new PropertyCache();
+
+
     protected final Map<File, Properties> propertiesFiles = Maps.newHashMap();
     protected final Map<String, String> definedNumbers = Maps.newHashMap();
 
@@ -134,4 +140,93 @@ public abstract class AbstractNumbersMojo extends AbstractMojo
      * Subclasses need to implement this method.
      */
     protected abstract void doExecute() throws Exception;
+
+    protected List<PropertyElement> loadPropertyElements()
+        throws Exception
+    {
+        final List<PropertyElement> propertyElements = Lists.newArrayList();
+        propertyElements.addAll(createNumbers(numbers));
+        propertyElements.addAll(createStrings(strings));
+        propertyElements.addAll(createDates(dates));
+
+        for (PropertyElement pe : propertyElements) {
+            if (pe.isExport()) {
+                final String value = pe.getPropertyValue();
+                if (value != null) {
+                    project.getProperties().setProperty(pe.getPropertyName(), value);
+                    LOG.info("Exporting Property name: %s, value: %s", pe.getPropertyName(), value);
+                }
+            }
+            else {
+                LOG.info("Property name: %s, value: %s", pe.getPropertyName(), pe.getPropertyValue());
+            }
+        }
+
+
+        return propertyElements;
+    }
+
+    private List<NumberField> createNumbers(final NumberDefinition [] numberDefinitions)
+        throws IOException
+    {
+        final List<NumberField> result = Lists.newArrayList();
+
+        if (!ArrayUtils.isEmpty(numberDefinitions)) {
+            for (NumberDefinition numberDefinition : numberDefinitions) {
+                numberDefinition.check();
+                final ValueProvider numberValue = propertyCache.getPropertyValue(numberDefinition);
+                final NumberField numberField = new NumberField(numberDefinition, numberValue);
+                result.add(numberField);
+            }
+        }
+        return result;
+    }
+
+    private List<StringField> createStrings(final StringDefinition[] stringDefinitions)
+        throws IOException
+    {
+        final List<StringField> result = Lists.newArrayList();
+
+        if (!ArrayUtils.isEmpty(stringDefinitions)) {
+            for (StringDefinition stringDefinition : stringDefinitions) {
+                stringDefinition.check();
+                final ValueProvider stringValue = propertyCache.getPropertyValue(stringDefinition);
+                final StringField stringField = new StringField(stringDefinition, stringValue);
+                result.add(stringField);
+            }
+        }
+        return result;
+    }
+
+    private List<DateField> createDates(final DateDefinition[] dateDefinitions)
+        throws IOException
+    {
+        final List<DateField> result = Lists.newArrayList();
+
+        if (!ArrayUtils.isEmpty(dateDefinitions)) {
+            for (DateDefinition dateDefinition : dateDefinitions) {
+                dateDefinition.check();
+                final ValueProvider dateValue = propertyCache.getPropertyValue(dateDefinition);
+                final DateField dateField = new DateField(dateDefinition, dateValue);
+                result.add(dateField);
+            }
+        }
+        return result;
+    }
+
+    private List<MacroField> createMacros(final MacroDefinition[] macroDefinitions)
+        throws IOException
+    {
+        final List<MacroField> result = Lists.newArrayList();
+
+        if (!ArrayUtils.isEmpty(macroDefinitions)) {
+            for (MacroDefinition macroDefinition : macroDefinitions) {
+                macroDefinition.check();
+                final ValueProvider macroValue = propertyCache.getPropertyValue(macroDefinition);
+                final MacroField macroField = new MacroField(macroDefinition, macroValue);
+                result.add(macroField);
+            }
+        }
+        return result;
+    }
 }
--- plugin/src/main/java/com/likeness/maven/plugins/numbers/GetNumbersMojo.java
+++ plugin/src/main/java/com/likeness/maven/plugins/numbers/GetNumbersMojo.java
@@ -1,15 +1,6 @@
 package com.likeness.maven.plugins.numbers;
 
-import java.io.IOException;
 import java.util.List;
-
-import com.likeness.maven.plugins.numbers.beans.DateDefinition;
-import com.likeness.maven.plugins.numbers.beans.StringDefinition;
-import org.apache.commons.lang3.ArrayUtils;
-
-import com.google.common.collect.Lists;
-import com.likeness.maven.plugins.numbers.beans.NumberDefinition;
-import com.likeness.maven.plugins.numbers.util.Log;
 
 
 /**
@@ -19,77 +10,11 @@ import com.likeness.maven.plugins.numbers.util.Log;
  */
 public class GetNumbersMojo extends AbstractNumbersMojo
 {
-    private final Log LOG = Log.findLog();
-
     private final PropertyCache propertyCache = new PropertyCache();
 
     protected void doExecute() throws Exception
     {
         LOG.debug("Running GetNumbers");
-        final List<PropertyElement> propertyElements = Lists.newArrayList();
-        propertyElements.addAll(createNumbers(numbers));
-        propertyElements.addAll(createStrings(strings));
-        propertyElements.addAll(createDates(dates));
-
-        for (PropertyElement pe : propertyElements) {
-            if (pe.isExport()) {
-                final String value = pe.getPropertyValue();
-                if (value != null) {
-                    project.getProperties().setProperty(pe.getPropertyName(), value);
-                    LOG.info("Exporting Property name: %s, value: %s", pe.getPropertyName(), value);
-                }
-            }
-            else {
-                LOG.info("Property name: %s, value: %s", pe.getPropertyName(), pe.getPropertyValue());
-            }
-        }
-    }
-
-    private List<NumberField> createNumbers(final NumberDefinition [] numberDefinitions)
-        throws IOException
-    {
-        final List<NumberField> result = Lists.newArrayList();
-
-        if (!ArrayUtils.isEmpty(numberDefinitions)) {
-            for (NumberDefinition numberDefinition : numberDefinitions) {
-                numberDefinition.check();
-                final ValueProvider numberValue = propertyCache.getPropertyValue(numberDefinition);
-                final NumberField numberField = new NumberField(numberDefinition, numberValue);
-                result.add(numberField);
-            }
-        }
-        return result;
-    }
-
-    private List<StringField> createStrings(final StringDefinition[] stringDefinitions)
-        throws IOException
-    {
-        final List<StringField> result = Lists.newArrayList();
-
-        if (!ArrayUtils.isEmpty(stringDefinitions)) {
-            for (StringDefinition stringDefinition : stringDefinitions) {
-                stringDefinition.check();
-                final ValueProvider stringValue = propertyCache.getPropertyValue(stringDefinition);
-                final StringField stringField = new StringField(stringDefinition, stringValue);
-                result.add(stringField);
-            }
-        }
-        return result;
-    }
-
-    private List<DateField> createDates(final DateDefinition[] dateDefinitions)
-        throws IOException
-    {
-        final List<DateField> result = Lists.newArrayList();
-
-        if (!ArrayUtils.isEmpty(dateDefinitions)) {
-            for (DateDefinition dateDefinition : dateDefinitions) {
-                dateDefinition.check();
-                final ValueProvider dateValue = propertyCache.getPropertyValue(dateDefinition);
-                final DateField dateField = new DateField(dateDefinition, dateValue);
-                result.add(dateField);
-            }
-        }
-        return result;
+        final List<PropertyElement> propertyElements = loadPropertyElements();
     }
 }
